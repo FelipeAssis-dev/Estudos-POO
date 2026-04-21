a038d20b11b2210470ede90ab3c03da43b723827
Felipe Alcantra de Assis <lipao01017@gmail.com>
2026-04-21 19:06:29 -0300

    Inicio dos estudos em POO, Exemplo com um programa de medição da área de um triangulo.

---
+++ src/application/Program.java
@@ -0,0 +1,43 @@
+
+package application;
+
+import entities.Triangle;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Triangle x, y;
+
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Medidas do Triangulo X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        System.out.println("Medidas do Triangulo   Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double p = (x.a + x.b + x.c) / 2.0;
+        double areaX = Math.sqrt( p * (p - x.a) * (p - x.b) * (p - x.c));
+
+        p = (y.a + y.b + y.c) / 2.0;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+        System.out.printf("Area do Triangulo X is %.2f\n", areaX);
+
+        System.out.printf("Area do Triangulo  Y is %.2f\n", areaY);
+
+        if(areaX > areaY){
+            System.out.println("Maior área: X");
+        }else {
+            System.out.println("Maior área: Y");
+        }
+        sc.close();
+    }
+}
